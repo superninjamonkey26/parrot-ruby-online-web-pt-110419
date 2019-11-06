3fea7d2cd2f0c38c0a7d1979fdec5ff2c23b4135
Monkey Mendel <stephanie.mendel26@gmail.com>
2019-11-06 14:44:46 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot("Pretty bird!")
+def parrot("Squawk!")
   puts "Squawk!"
   "Squawk!"
 end
-parrot
+parrot("Pretty bird!")
